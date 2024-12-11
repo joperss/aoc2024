6986cc5176ba79e719faf560a3842884d9874176
Johan Persson <persson.johan.92@gmail.com>
2024-12-11 22:39:36 +0100

    Day 2, problem 2

--- day2/problem1.py
+++ day2/problem1.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 import csv
-from enum import Enum
-
-Order = Enum('Order', [('ASC', 1), ('DESC', 2)])
 
 def is_safe_asc(report):
-    return all(j - i >= 1 and j - i <=3 for i, j in zip(report, report[1:]))
+    return all(1 <= j - i <= 3 for i, j in zip(report, report[1:]))
 
 def is_safe_desc(report):
-    return all(j - i <= -1 and j - i >=-3 for i, j in zip(report, report[1:]))
+    return all(-1 >= j - i >= -3 for i, j in zip(report, report[1:]))
 
-with open("input.txt", newline='') as reports:
-    reader = csv.reader(reports, delimiter=' ')
-    
-    safe_reports = 0
-    for row in reader:
-        report = [int(i) for i in row]
-        safe = is_safe_asc(report) or is_safe_desc(report)
-        safe_reports = safe_reports + 1 if safe else 0
+def main():
+    with open("input.txt", newline='') as reports:
+        reader = csv.reader(reports, delimiter=' ')
 
-    print("Safe reports: " + str(safe_reports))
+        safe_reports = 0
+        for row in reader:
+            report = [int(i) for i in row]
+            safe = is_safe_asc(report) or is_safe_desc(report)
+            safe_reports += 1 if safe else 0
+
+        print("Safe reports: " + str(safe_reports))
+
+if __name__ == '__main__':
+    main()
--- day2/problem2.py
+++ day2/problem2.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 import csv
-import itertools
-from enum import Enum
-
-Order = Enum('Order', [('ASC', 1), ('DESC', 2)])
 
 def is_safe_asc(report):
-    return all(j - i >= 1 and j - i <=3 for i, j in zip(report, report[1:]))
+    return all(1 <= j - i <= 3 for i, j in zip(report, report[1:]))
 
 def is_safe_desc(report):
-    return all(j - i <= -1 and j - i >=-3 for i, j in zip(report, report[1:]))
+    return all(-1 >= j - i >= -3 for i, j in zip(report, report[1:]))
 
 def is_safe_dampened(report):
     for i in range(len(report)):
@@ -18,13 +14,17 @@ def is_safe_dampened(report):
             return True
     return False
 
-with open("input.txt", newline='') as reports:
-    reader = csv.reader(reports, delimiter=' ')
-    
-    safe_reports = 0
-    for row in reader:
-        report = [int(i) for i in row]
-        safe = is_safe_asc(report) or is_safe_desc(report) or is_safe_dampened(report)
-        safe_reports = safe_reports + 1 if safe else 0
+def main():
+    with open("input.txt", newline='') as reports:
+        reader = csv.reader(reports, delimiter=' ')
 
-    print("Safe reports: " + str(safe_reports))
+        safe_reports = 0
+        for row in reader:
+            report = [int(i) for i in row]
+            safe = is_safe_asc(report) or is_safe_desc(report) or is_safe_dampened(report)
+            safe_reports += 1 if safe else 0
+
+        print("Safe reports: " + str(safe_reports))
+
+if __name__ == '__main__':
+    main()
